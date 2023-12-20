ffcc0aef0f8c416e8f6a11163d569e29c591884d
ZHANG-XIAN-SHENG1 <102649009+ZHANG-XIAN-SHENG1@users.noreply.github.com>
2023-12-20 11:51:56 +0800

    Update mediaData.js

--- mediaData.js
+++ mediaData.js
@@ -2,7 +2,7 @@ var mediaData = {
     authorPath:"images/",
     authorspicUrl:["李建宏老师.jpg","zcw.png","zwy.jpg","xjw.jpg","zdn.jpg","zlf.jpg","glj.jpg"],
     authorsnames:["李建宏老师","zcw","zwy","xjw","zdn","zlf","glj"],
-    authorsUrl:[" " , "https://zhong-wenyu.github.io/" , "https://zhang-xian-sheng1.github.io", "https://xujiangwe.github.io", "https://z-dn.github.io", "https://zenglingfengzeta.github.io", "https://Guo-lijing.github.io" ],
+    authorsUrl:[" " , "https://zhang-xian-sheng1.github.io",  "https://zhong-wenyu.github.io/" ,"https://xujiangwe.github.io", "https://z-dn.github.io", "https://zenglingfengzeta.github.io", "https://Guo-lijing.github.io" ],
     audioPath : "https://jxzy-dmxy.jxstnu.edu.cn/",
     audioFiles : [
         {fileName:["linuxCMD-6.mp3"],
